bf8af78af8285665b40754650a1c6a0794aa8ef1
Nick <fick.nomin@gmail.com>
2026-03-30 13:11:23 +0100

    Initialise project, configure next-config.ts, install Shadcn and configure tailwind.config.ts, install react query and create a Provider.tsx wrapper for the QueryClientProvider

---
+++ src/components/Provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+
+export const Provider = ({ children }: { children: React.ReactNode }) => {
+
+  const [queryClient] = useState(() => new QueryClient(
+    {
+      defaultOptions: {
+        queries: {
+          staleTime: 1000 * 60 * 3, // 3 minutes stale time for games
+          retry: 1,
+          refetchOnWindowFocus: false,
+        }
+      }
+    }
+  ));
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
